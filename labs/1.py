2531c49a7a7ece9da658637e44a4647c5e9ed9c1
Adidale <guitarcountry11@gmail.com>
2025-09-20 11:48:51 +0300

    modified labs 1 and 2

--- labs/1.py
+++ labs/1.py
@@ -1,11 +1,11 @@
 # Вариант 6
 
 # collecting user data
-name = input('Your name: ')
-favorite_subject = input ('Your favorite subject at MAI: ')
-group_number = int(input('Your group number: '))
+name = input('Ваше имя?')
+favorite_subject = input ('Какой Ваш любимый предмет в МАИ?')
+group_number = int(input('В какой группе Вы учитесь?'))
 
 # showing user data on console
-print('Your name:', name)
-print('Your favorite subject at MAI:', favorite_subject)
-print('You study in group:', group_number)
+print('Ваше имя:', name)
+print('Ваш любимый предмет в МАИ:', favorite_subject)
+print('Вы учитесь в группе номер:', group_number)
--- labs/2.py
+++ labs/2.py
@@ -1,8 +1,11 @@
+# Variant 21
 import math
 
+# colleting data
 a = float(input('enter a: '))
 b = float(input('enter b: '))
 c = float(input('enter c: '))
 
+# calculating t and printing it
 t = ((a**-1 + b**-2 + c**-3) / (math.pi * abs(a * b) - c)) + ((math.e**c + math.cos(b)) / (math.cos(c)))
 print(t)
