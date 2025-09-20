77173d855163d8185e62986c1983566c1fee9dcb
Adidale <adidale@MacBook-Air-de-Julio.local>
2025-09-20 17:26:49 +0300

    lab 2

--- labs/2.py
+++ labs/2.py
@@ -9,6 +9,6 @@ a = float(input('enter a: '))
 b = float(input('enter b: '))
 c = float(input('enter c: '))
 
-# calculating t and printing it
+# calculating t and showing it
 t = ((a**-1 + b**-2 + c**-3) / (math.pi * abs(a * b) - c)) + ((math.e**c + math.cos(b)) / (math.cos(c)))
 print("{:.2f}".format(t))
