7a41b73cf9f9c0db8d9d7b09ee71befe0b253993
Adidale <adidale@MacBook-Air-de-Julio.local>
2025-10-02 23:18:56 +0300

    lab work 3-1 and 3-2

---
+++ labs/3-1.py
@@ -0,0 +1,33 @@
+# excercise 5
+import math
+
+# collecting data
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+d = int(input('Enter d: '))
+
+# finding the smallest number
+if a<b:
+  if a<c:
+    if a<d:
+      minN = a
+
+if b<a:
+  if b<c:
+    if b<d:
+      minN = b
+
+if c<a:
+  if c<b:
+    if c<d:
+      minN = c
+
+if d<a:
+  if d<b:
+    if d<c:
+      minN = d
+
+# showing data
+print(minN)
+print(math.cos(minN))
---
+++ labs/3-2.py
@@ -0,0 +1,12 @@
+# collecting data
+n = int(input('Enter month (1-12): '))
+
+# months array
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'Jule', 'August', 'September', 'October', 'Novermber', 'December']
+
+# validating data
+if n < 1 or n > 12:
+  print('Invalid month')
+else:
+  # showing month
+  print(months[n - 1])
