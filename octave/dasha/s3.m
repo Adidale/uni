773280d203be797deff0edc9505de38c9d76036b
Adidale <guitarcountry11@gmail.com>
2026-06-04 21:14:39 +0300

    a

--- octave/dasha/s3.m
+++ octave/dasha/s3.m
@@ -6,14 +6,14 @@ q = 2.6;
 
 syms XA YA XB YB XC YC MC;
 
-FQ = [q*4 0 0];MM = [0 0 M];
-MMCL = [0 0 MC];
-MMCR = [0 0 -MC];
+FQ = [q*4 0 0];
+MM = [0 0 M];
 
 FRA = [XA YA 0];
 FRB = [XB YB 0];
-FRCL = [XC YC 0];
-FRCR = [-XC -YC 0];
+FRC1 = [XC YC 0];
+FRC2 = [XC 0 0];
+MMCR2 = [0 0 MC];
 
 A = [0 0 0];
 B = [5 1 0];
@@ -22,12 +22,12 @@ D = [3.5 2.5 0];
 E = [3.5 1.5 0];
 G = [0 2 0];
 
-for i = 1:length(P1)  
+for i = 1:length(P1)
   FP1= [-P1(i)*cosd(45) -P1(i)*sind(45) 0];
-  EQFL1 = FQ + FRA + FRCL == [0 0 0];
-  EQML1 = cross(G,FQ) + cross (A, FRA) + cross (C, FRCL) + MM == [0 0 0];
-  EQFR1 = FRB + FRCR + FP1 == [0 0 0];
-  EQMR1 = cross(D,FP1) + cross(B,FRB) + cross(C,FRCR) == [0 0 0];
+  EQFL1 = FQ + FRA + FRC1 == [0 0 0];
+  EQML1 = cross(G,FQ) + cross (A, FRA) + cross (C, FRC1) + MM == [0 0 0];
+  EQFR1 = FRB - FRC1 + FP1 == [0 0 0];
+  EQMR1 = cross(D,FP1) + cross(B,FRB) - cross(C,FRC1) == [0 0 0];
 
   Sys1= [EQFL1(1) EQFL1(2) EQFR1(1) EQFR1(2) EQML1(3) EQMR1(3)];
   solution1 = vpasolve(Sys1, [XA YA XB YB XC YC]);
@@ -42,23 +42,22 @@ for i = 1:length(P1)
   rb1(i) = sqrt(xb1(i)^2 + yb1(i)^2);
   rc1(i) = sqrt(xc1(i)^2 + yc1(i)^2);
 
-  EQFL2 = FQ + FRA + FRCL == [0 0 0];
-  EQML2 = cross(G,FQ) + cross (A, FRA) + cross (C, FRCL) + MM == [0 0 0];
-  EQFR2 = FRB + FRCR + FP1 == [0 0 0];
-  EQMR2 = cross(D,FP1) + cross(B,FRB) + cross(C,FRCR) == [0 0 0];
+  EQFL2 = FQ + FRA + FRC2 == [0 0 0];
+  EQML2 = cross(G,FQ) + cross (A, FRA) + cross (C, FRC2) + MM + MMCR2 == [0 0 0];
+  EQFR2 = FRB + FRC2 + FP1 == [0 0 0];
+  EQMR2 = cross(D,FP1) + cross(B,FRB) - cross(C,FRC2) - MMCR2 == [0 0 0];
 
   Sys2= [EQFL2(1) EQFL2(2) EQFR2(1) EQFR2(2) EQML2(3) EQMR2(3)];
-  solution2 = vpasolve(Sys2, [XA YA XB YB XC YC]);
+  solution2 = vpasolve(Sys2, [XA YA XB YB XC MC]);
 
   xa2(i) = eval(solution2(1));
   ya2(i) = eval(solution2(2));
   xb2(i) = eval(solution2(3));
   yb2(i) = eval(solution2(4));
   xc2(i) = eval(solution2(5));
-  yc2(i) = eval(solution2(6));
+  mc2(i) = eval(solution2(6));
   ra2(i) = sqrt(xa2(i)^2 + ya2(i)^2);
   rb2(i) = sqrt(xb2(i)^2 + yb2(i)^2);
-  rc2(i) = sqrt(xc2(i)^2 + yc2(i)^2);
 endfor
 
 
@@ -108,14 +107,17 @@ subplot(3,4,3)
 plot(P1,ra2)
 title('RA2')
 
-
 subplot(3,4,7)
 plot(P1,rb2)
 title('RB2')
 
-subplot(3,4,10)
-plot(P1,yc2)
-title('YC2')
+subplot(3,4,9)
+plot(P1,xc2)
+title('XC2')
+
+subplot(3,4,11)
+plot(P1,mc2)
+title('MC2')
 
 printf('bearing\n');
 printf('XA=%f ', xa1(end));
@@ -135,4 +137,5 @@ printf('RA=%f\n',ra2(end));
 printf('XB=%f ', xb2(end));
 printf('YB=%f ', yb2(end));
 printf('RB=%f\n',rb2(end));
-printf('YC=%f ', yc2(end));
+printf('XC=%f ', xc2(end));
+printf('MC=%f ', mc2(end));
